a43dc6d2b527b4f86502e8c0483ca29285992f4c
dikdiktasdik <dikdik.zahirra@gmail.com>
2013-03-13 22:12:11 +0700

    add user role

--- application/views/role_details/frm_role_detail.php
+++ application/views/role_details/frm_role_detail.php
@@ -24,57 +24,63 @@
     }
 </script>
 <div class="wrap">
-    <div class="row-fluid">
-        <div class="span6">
-            <h3>Form Roled Detail</h3>
-            <table class="table table-bordered">
-                <thead>
+    <form action="" method="get">
+        <div class="row-fluid">
+            <div class="span6">
+                <h3>Form Roled Detail</h3>
+
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th colspan="4" align="center">
+                                <input type="checkbox" onclick="check_all(this)" name="all_module" id="all_module"/> Roled Module
+                            </th>
+                        </tr>
+                    </thead>
                     <tr>
-                        <th colspan="4" align="center">
-                            <input type="checkbox" onclick="check_all(this)" name="all_module" id="all_module"/> Roled Module
-                        </th>
+                        <td><input type="checkbox" name="module[]" value="Branches"/> Branch</td>
+                        <td><input type="checkbox" name="module[]" value="Departements"/> Departement</td>
+                        <td><input type="checkbox" name="module[]" value="Tax_Employees"/> Tax Employee</td>
+                        <td><input type="checkbox" name="module[]" value="Employee_Status"/> Employee Status</td>
                     </tr>
-                </thead>
-                <tr>
-                    <td><input type="checkbox" name="module[]" value="Branches"/> Branch</td>
-                    <td><input type="checkbox" name="module[]" value="Departements"/> Departement</td>
-                    <td><input type="checkbox" name="module[]" value="Tax_Employees"/> Tax Employee</td>
-                    <td><input type="checkbox" name="module[]" value="Employee_Status"/> Employee Status</td>
-                </tr>
-                <tr>
-                    <td><input type="checkbox" name="module[]" value="Marital_Status"/> Marital Status</td>
-                    <td><input type="checkbox" name="module[]" value="Titles"/> Title</td>
-                    <td><input type="checkbox" name="module[]" value="Components"/> Component(Gaji)</td>
-                    <td><input type="checkbox" name="module[]" value="Salaries"/> Salary</td>
-                </tr>
-                <tr>
-                    <td><input type="checkbox" name="module[]" value="Staffs"/> Staff</td>
-                    <td><input type="checkbox" name="module[]" value="Assets"/> Assets</td>
-                    <td><input type="checkbox" name="module[]" value="Users"/> Users</td>
-                    <td><input type="checkbox" name="module[]" value="Role_Details"/> Roled</td>
-                </tr>
-            </table>
-        </div>
-    </div>
-    <div class="row-fluid" style="margin-top: 20px;">
-        <div class="span6">
-            <table class="table table-bordered">
-                <thead>
                     <tr>
-                        <th colspan="4" align="center"><input type="checkbox" name="all_privileges" id="all_privileges"/>All Privileges</th>
+                        <td><input type="checkbox" name="module[]" value="Marital_Status"/> Marital Status</td>
+                        <td><input type="checkbox" name="module[]" value="Titles"/> Title</td>
+                        <td><input type="checkbox" name="module[]" value="Components"/> Component(Gaji)</td>
+                        <td><input type="checkbox" name="module[]" value="Salaries"/> Salary</td>
                     </tr>
-                </thead>
-                <tr>
-                    <td><input type="checkbox" name="privileges1"/> INSERT</td>
-                    <td><input type="checkbox" name="privileges2"/> UPDATE</td>
-                    <td><input type="checkbox" name="privileges3"/> DELETE</td>
-                    <td><input type="checkbox" name="privileges4"/> APPROVAL</td>
-                </tr>
-            </table>
+                    <tr>
+                        <td><input type="checkbox" name="module[]" value="Staffs"/> Staff</td>
+                        <td><input type="checkbox" name="module[]" value="Assets"/> Assets</td>
+                        <td><input type="checkbox" name="module[]" value="Users"/> Users</td>
+                        <td><input type="checkbox" name="module[]" value="Role_Details"/> Roled</td>
+                    </tr>
+                </table>
+            </div>
         </div>
-    </div>
-    <div class="well-small">
-        <?php echo form_submit($btn_save); ?>
-    </div>
+        <div class="row-fluid" style="margin-top: 20px;">
+            <div class="span6">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th colspan="4" align="center">
+                                <input type="checkbox" name="all_privileges" id="all_privileges"/> All Privileges
+                            </th>
+                        </tr>
+                    </thead>
+                    <tr>
+                        <td><input type="checkbox" name="privileges1"/> INSERT</td>
+                        <td><input type="checkbox" name="privileges2"/> UPDATE</td>
+                        <td><input type="checkbox" name="privileges3"/> DELETE</td>
+                        <td><input type="checkbox" name="privileges4"/> APPROVAL</td>
+                    </tr>
+                </table>
+
+            </div>
+        </div>
+        <div class="well-small">
+            <?php echo form_submit($btn_save); ?>
+        </div>
+    </form>
 </div>
 <?php get_footer(); ?>
