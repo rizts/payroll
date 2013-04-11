10ad66eacc113b52458718dd208e18080a01705c
Dikdik Tasdik Laksana <dikdik.zahirra@gmail.com>
2013-04-11 14:01:26 +0700

    update form field setting

--- application/views/settings/frm_settings.php
+++ application/views/settings/frm_settings.php
@@ -14,19 +14,50 @@
         <?php echo form_open($form_action) . form_hidden('id', $id); ?>
         <table width="100%">
             <tr>
-                <td width="20%">Name</td>
+                <td width="20%">Company Name</td>
                 <td>
-                    <div class="span3"><?php echo form_input($name); ?></div>
+                    <div class="span3"><?php echo form_input($company_name); ?></div>
                 </td>
             </tr>
             <tr>
-                <td width="20%">Value</td>
+                <td width="20%" valign="top">Address</td>
                 <td>
-                    <div class="span3"><?php echo form_input($value); ?></div>
+                    <div class="span3"><?php echo form_textarea($address); ?></div>
                 </td>
             </tr>
+            <tr>
+                <td width="20%">Phone</td>
+                <td>
+                    <div class="span3"><?php echo form_input($phone); ?></div>
+                </td>
+            </tr>
+            <tr>
+                <td width="20%">Fax</td>
+                <td>
+                    <div class="span3"><?php echo form_input($fax); ?></div>
+                </td>
+            </tr>
+            <tr>
+                <td width="20%">Email</td>
+                <td>
+                    <div class="span3"><?php echo form_input($email); ?></div>
+                </td>
+            </tr>
+            <tr>
+                <td width="20%">City</td>
+                <td>
+                    <div class="span3"><?php echo form_input($city); ?></div>
+                </td>
+            </tr>
+            <tr>
+                <td width="20%">No NPWP</td>
+                <td>
+                    <div class="span3"><?php echo form_input($no_npwp); ?></div>
+                </td>
+            </tr>
+
         </table>
-        <?php echo form_submit($btn_save); ?> <?php echo $link_back; ?>
+        <?php echo form_submit($btn_save); ?>
         <?php echo form_close() ?>
     </div>
 </div>
