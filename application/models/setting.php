bbb9b96f9d84c777c6d2547298807abbc626245f
Dikdik Tasdik Laksana <dikdik.zahirra@gmail.com>
2013-04-12 11:32:50 +0700

    update model setting

--- application/models/setting.php
+++ application/models/setting.php
@@ -14,10 +14,30 @@ class Setting extends DataMapper {
         parent::__construct();
     }
 
-    function _delete($id) {
-        $this->db->where('id', $id);
-        $this->db->delete($this->table);
-    }
+
+     function update($k, $v){
+        $this->db->like('name', $k);
+        $this->db->from($this->table);
+        $count = $this->db->count_all_results();
+        if($count==0){
+          $this->db->insert($this->table, array("name"=>$k, "val"=>$v));
+        }else{
+          $this->db->update($this->table, array("name"=>$k, "val"=>$v), array("name"=>$k));
+        }
+      }
+      
+      function get_val($key){
+        $setting = new Setting();
+        //$query = $this->db->get_where($this->table, array("name" => $key));
+        //if ($query->num_rows() > 0){
+        //$row = $query->row();
+        //return $row->val;
+        //}
+        $result = $setting->where("name", $key)->get();
+        return $result->val;
+        
+      }
+
 
 }
 
